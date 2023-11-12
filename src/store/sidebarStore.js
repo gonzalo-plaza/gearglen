51c2d759fbb6c8c20e82ef8245e58d310e78bdc4
gonzalo-plaza <plazaruedag@gmail.com>
2023-11-12 18:46:31 +0100

    SidebarStore: Add functionality to block scroll in mobile

--- src/store/sidebarStore.js
+++ src/store/sidebarStore.js
@@ -3,8 +3,16 @@ import { create } from 'zustand'
 export const useSidebarStore = create(set => (
     {
         showSidebar: false,
-        launchSidebar() {set({showSidebar: true})},
-        hideSidebar() {set({showSidebar: false})},
+        launchSidebar() {
+            const body= document.getElementsByTagName('body');
+            body[0].classList.add('scroll-block');
+            set({showSidebar: true});
+        },
+        hideSidebar() {
+            const body= document.getElementsByTagName('body');
+            body[0].classList.remove('scroll-block');
+            set({showSidebar: false})
+        },
     }
 )
 )
