f947bf3b9558810f1b20f1fa4c2598c70b284e78
gonzalo-plaza <plazaruedag@gmail.com>
2023-11-18 11:15:58 +0100

    Section description: Add section description

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
       <div className='main-container'>
         <Header />
         <main role='main' className='main'>
-          <section>
+          <section className='home-section'>
             <h1 className='section__title'>Tu Vehículo en Forma</h1>
             <div className='section__title-line'></div>
             <img className='section__img' src='./images/vehicles-collection.png' />
@@ -18,7 +18,13 @@ function App() {
             <h1 className='section__title'>Mantén tu Vehículo Siempre Listo</h1>
             <div className='section__title-line'></div>
             <img className='section__img' src='./images/car-maintenance.png' />
+            <p className='section__description'>
+              Disfruta de la comodidad de tener tu vehículo en perfecto estado con GearGlen. Gestiona el mantenimiento y las reparaciones con nuestro sistema completo que garantiza la eficiencia y seguridad de cada viaje.
+            </p>
           </section>
+          <footer>
+            
+          </footer>
         </main>
       </div>
       <Sidebar />
