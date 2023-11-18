e9532e82d257d604cbb3993414489e6c4698b09f
gonzalo-plaza <plazaruedag@gmail.com>
2023-11-18 10:47:05 +0100

    InfoCard: Create infoCard component, add the images and create a HomeInfoCards component

--- src/components/infoCard/InfoCard.jsx
+++ src/components/infoCard/InfoCard.jsx
@@ -1,11 +1,11 @@
-export const InfoCard = ({src,alt, title, description}) => {
-    return(
-        <section className='info-card'>
-        <img src={src} alt={alt} />
-        <div className='info-card-description'>
-          <h2 className='info-card-description__img'>{title}</h2>
-          <p className='info-card-description__text'>{description}</p>
-        </div>
-      </section>
-    )
+export const InfoCard = ({ src, alt, title, description }) => {
+  return (
+    <article className='info-card'>
+      <img src={src} alt={alt} className='info-card__img'/>
+      <div className='info-card-description'>
+        <h2 className='info-card-description__title'>{title}</h2>
+        <p className='info-card-description__text'>{description}</p>
+      </div>
+    </article>
+  )
 }
