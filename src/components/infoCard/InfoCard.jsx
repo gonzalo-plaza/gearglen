bb4b74709843d77213468428efefe7d4ae0d4a62
gonzalo-plaza <plazaruedag@gmail.com>
2023-11-19 17:44:44 +0100

    HomeView: Adapt sections and homeInfoCards to desktop view

--- src/components/infoCard/InfoCard.jsx
+++ src/components/infoCard/InfoCard.jsx
@@ -1,7 +1,10 @@
-export const InfoCard = ({ src, alt, title, description }) => {
+export const InfoCard = ({ src, desktopSrc, alt, title, description }) => {
   return (
     <article className='info-card'>
-      <img src={src} alt={alt} className='info-card__img'/>
+      <picture className="info-card-picture">
+        <source srcset={desktopSrc} media="(min-width: 1024px)" />
+        <img className='info-card-picture__img' src={src} alt={alt} />
+      </picture>
       <div className='info-card-description'>
         <h2 className='info-card-description__title'>{title}</h2>
         <p className='info-card-description__text'>{description}</p>
