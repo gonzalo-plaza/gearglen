c66d66beb895eec38c59826ce0c86b18bab2d3c0
gonzalo-plaza <plazaruedag@gmail.com>
2023-12-04 18:43:46 +0100

    PropTypes: Declare proptypes form components props

--- src/components/footer/FooterSectionText.jsx
+++ src/components/footer/FooterSectionText.jsx
@@ -1,3 +1,5 @@
+import PropTypes from 'prop-types';
+
 export const FooterSectionText = ({ Icon, text }) => {
 	const classNameResult = `footer-section__text ${Icon ? 'has-icon' : ''}`;
 	return (
@@ -6,4 +8,9 @@ export const FooterSectionText = ({ Icon, text }) => {
 			<span>{text}</span>
 		</p>
 	);
+};
+
+FooterSectionText.propTypes = {
+	Icon: PropTypes.element,
+	text: PropTypes.string.isRequired,
 };
