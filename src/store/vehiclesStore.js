25fa0886d6b18e294234e878c4b819e854cfb2e9
gonzalo-plaza <plazaruedag@gmail.com>
2023-12-04 18:31:03 +0100

    Fix format

--- src/store/vehiclesStore.js
+++ src/store/vehiclesStore.js
@@ -1,28 +1,28 @@
-import { create } from "zustand";
-import { persist } from "zustand/middleware";
+import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
 
 export const useVehiclesStore = create(
-    persist(
-        (set, get) => ({
-            vehicles: [],
-            addVehicle(vehicleObject) {
-                const { vehicles } = get()
-                const newVehicles = structuredClone(vehicles)
-                newVehicles.push(vehicleObject)
-                set({ vehicles: newVehicles })
-            },
-            removeVehicle(vehicleId) {
-                const { vehicles } = get()
-                const actualVehicles = structuredClone(vehicles)
-                const vehiclesCleaned = actualVehicles.filter((vehicle) => {
-                    return vehicle.id !== vehicleId;
-                })
+	persist(
+		(set, get) => ({
+			vehicles: [],
+			addVehicle(vehicleObject) {
+				const { vehicles } = get();
+				const newVehicles = structuredClone(vehicles);
+				newVehicles.push(vehicleObject);
+				set({ vehicles: newVehicles });
+			},
+			removeVehicle(vehicleId) {
+				const { vehicles } = get();
+				const actualVehicles = structuredClone(vehicles);
+				const vehiclesCleaned = actualVehicles.filter(vehicle => {
+					return vehicle.id !== vehicleId;
+				});
 
-                set({ vehicles: vehiclesCleaned });
-            }
-        }),
-        {
-            name: 'vehicles',
-        }
-    )
+				set({ vehicles: vehiclesCleaned });
+			},
+		}),
+		{
+			name: 'vehicles',
+		},
+	),
 );
