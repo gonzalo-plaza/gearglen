76116d345c4b5ad5f8005229fc9eda747823cfb0
gonzalo-plaza <plazaruedag@gmail.com>
2023-12-05 11:31:26 +0100

    Vehicle Card: Move vehicle card to a component

---
+++ src/components/vehicleCard/VehicleCard.jsx
@@ -0,0 +1,104 @@
+import { useVehiclesStore } from '../../store/vehiclesStore';
+import {
+	MdCopyright,
+	MdExplore,
+	MdLocalGasStation,
+	MdMenuBook,
+	MdCalendarMonth,
+	MdPin,
+	MdDelete,
+} from 'react-icons/md';
+
+import PropTypes from 'prop-types';
+
+export const VehicleCard = ({ vehicle }) => {
+	const { removeVehicle } = useVehiclesStore();
+	const handleRemove = id => () => {
+		removeVehicle(id);
+	};
+	return (
+		<article key={vehicle.id.value} className='vehicle-card'>
+			<header className='vehicle-card__header'>
+				<img
+					className='vehicle-card__img'
+					src={`./images/vehicle-${vehicle.type.value}.png`}
+				/>
+				<button
+					className='remove-vehicle-button'
+					onClick={handleRemove(vehicle.id.value)}
+				>
+					<MdDelete className='remove-vehicle-button__icon' />
+				</button>
+			</header>
+			<div>
+				<h3 className='vehicle-card__title'>{vehicle.name.value}</h3>
+				<ul className='vehicle-card-detail-list'>
+					<li className='vehicle-card-detail'>
+						<MdCopyright className='vehicle-card-detail__icon' />
+						<span
+							className={`vehicle-card-detail__description ${
+								vehicle.brand.noAdded ? 'no-added' : ''
+							}`}
+						>
+							{vehicle.brand.value}
+						</span>
+					</li>
+					<li className='vehicle-card-detail'>
+						<MdPin className='vehicle-card-detail__icon' />
+						<span
+							className={`vehicle-card-detail__description ${
+								vehicle.licensePlate.noAdded ? 'no-added' : ''
+							}`}
+						>
+							{vehicle.licensePlate.value}
+						</span>
+					</li>
+					<li className='vehicle-card-detail'>
+						<MdExplore className='vehicle-card-detail__icon' />
+						<span
+							className={`vehicle-card-detail__description ${
+								vehicle.kilometers.noAdded ? 'no-added' : ''
+							}`}
+						>
+							{vehicle.kilometers.value}
+						</span>
+					</li>
+					<li className='vehicle-card-detail'>
+						<MdMenuBook className='vehicle-card-detail__icon' />
+						<span
+							className={`vehicle-card-detail__description ${
+								vehicle.model.noAdded ? 'no-added' : ''
+							}`}
+						>
+							{vehicle.model.value}
+						</span>
+					</li>
+					<li className='vehicle-card-detail'>
+						<MdCalendarMonth className='vehicle-card-detail__icon' />
+						<span
+							className={`vehicle-card-detail__description ${
+								vehicle.date.noAdded ? 'no-added' : ''
+							}`}
+						>
+							{vehicle.date.value}
+						</span>
+					</li>
+					<li className='vehicle-card-detail'>
+						<MdLocalGasStation className='vehicle-card-detail__icon' />
+						<span
+							className={`vehicle-card-detail__description ${
+								vehicle.fuel.noAdded ? 'no-added' : ''
+							}`}
+						>
+							{vehicle.fuel.value}
+						</span>
+					</li>
+				</ul>
+			</div>
+		</article>
+	);
+};
+
+VehicleCard.propTypes = {
+	vehicle: PropTypes.object.isRequired,
+};
