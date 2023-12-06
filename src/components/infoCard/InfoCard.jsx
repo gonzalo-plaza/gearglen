c0a4a96d6143b2246def4a59a30403216cd5d6f2
gonzalo-plaza <plazaruedag@gmail.com>
2023-12-06 13:24:11 +0100

    Info Card Image: Set with and height in info card image to improve the performance

--- src/components/infoCard/InfoCard.jsx
+++ src/components/infoCard/InfoCard.jsx
@@ -4,9 +4,20 @@ export const InfoCard = ({ src, desktopSrc, alt, title, description }) => {
 	return (
 		<article className='info-card'>
 			<picture className='info-card-picture'>
-				<source srcSet={desktopSrc} media='(min-width: 1024px)' />
+				<source
+					srcSet={desktopSrc}
+					width='900'
+					height='600'
+					media='(min-width: 1024px)'
+				/>
 
-				<img className='info-card-picture__img' src={src} alt={alt} />
+				<img
+					className='info-card-picture__img'
+					width='220'
+					height='220'
+					src={src}
+					alt={alt}
+				/>
 			</picture>
 			<div className='info-card-description'>
 				<h2 className='info-card-description__title'>{title}</h2>
